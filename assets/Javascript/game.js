c4187801ddcd48995976db6fcde864b300604dc4
Jonathan Boesel <jboesel@vt.edu>
2019-03-19 21:18:02 +0000

    quick cleanup/tweaking

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -11,7 +11,6 @@ var computerChoices = [
 var computerPick = computerChoices[Math.floor(Math.random() * computerChoices.length)];
 // A loop is run for the length of the chosen phrase to write the blanks for the selected phrase
 var guessedLetters = [];
-console.log(guessedLetters);
 var answerArray = [];
 var missesArray = [];
 var guessesLeft = 9;
@@ -25,7 +24,6 @@ for (var i = 0; i < computerPick.length; i++) {
     answerArray[i] = "_";
 }
 document.getElementById("hangword").innerHTML = answerArray.join(" ")
-console.log(answerArray);
 
 function reset() {
     computerPick = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -40,64 +38,43 @@ function reset() {
 
     document.getElementById("guessesleft").innerHTML = "Guesses remaining: " + guessesLeft;
     document.getElementById("hangword").innerHTML = answerArray.join(" ");
+    document.getElementById("misses").innerHTML = missesArray
 }
 
 // User inputs a guess
 document.onkeyup = function(event) {
     var userGuess = event.key;
-    var letter = String.fromCharCode(event.keyCode).toLowerCase();
-    var tmpArray = [];
-    // var aLetter = /^[a-z]+$/;
-    // if (letter !== /^[a-z]+$/) {
-    //     return;
-    // }
-    for (var i = 0; i < computerPick.length; i++) {
-        if (computerPick[i] === letter) {
-            answerArray[i] = letter;
-            guessedLetters.push(letter);
-            var correct = true;
+    //first if statement prevents keystrokes after a game is won + duplicate guesses
+    if ((computerPick.join("") !== answerArray.join("")) && !guessedLetters.includes(userGuess)) {
+        var letter = String.fromCharCode(event.keyCode).toLowerCase();
+        var tmpArray = [];
+        for (var i = 0; i < computerPick.length; i++) {
+            if (computerPick[i] === letter) {
+                answerArray[i] = letter;
+                guessedLetters.push(letter);
+                var correct = true;
+            }
         }
-    }
-    if (correct != true) {
-        // alert("wrong");
-        missesArray.push(letter);
-        guessedLetters.push(letter);
-        guessesLeft--;
-    }
+        if (correct != true) {
+            // alert("wrong");
+            missesArray.push(letter);
+            guessedLetters.push(letter);
+            guessesLeft--;
+        }
 
-    if (guessesLeft <= 0) {
-        losesTotal++;
-        reset();
-    }
+        if (guessesLeft <= 0) {
+            losesTotal++;
+            reset();
+        }
 
-    if (computerPick.join("") === answerArray.join("")) {
-        winsTotal++;
-        reset();
-    }
-
-
-
-    // if ((answerArray[i] === computerPick[i]) || (answerArray[i] === aLetter) || (answerArray[i] === letter)) {
-    //         tmpArray.push(computerPick[i]);
-    //     }
-    //     else if ((computerPick[i] === letter) || (answerArray[i] === letter)) {
-    //         tmpArray.push(letter);
-    //     }
-    //     else {
-    //         tmpArray.push("_");
-    //     }
-    // }
-
-    // answerArray = tmpArray.join(" ");
-    console.log(answerArray);
-    document.getElementById("loses").innerHTML = "Loses: " + losesTotal;
-    document.getElementById("wins").innerHTML = "Wins: " + winsTotal;
-    document.getElementById("guessesleft").innerHTML = "Guesses remaining: " + guessesLeft;
-    document.getElementById("hangword").innerHTML = answerArray.join(" ");
-    document.getElementById("misses").innerHTML = missesArray.join(" ");
-
-    console.log(tmpArray);
-    console.log(computerPick);
-    // console.log(missesArray);
-
-};
+        if (computerPick.join("") === answerArray.join("")) {
+            winsTotal++;
+            setTimeout(function() { reset(); }, 2000);
+        }
+        document.getElementById("loses").innerHTML = "Loses: " + losesTotal;
+        document.getElementById("wins").innerHTML = "Wins: " + winsTotal;
+        document.getElementById("guessesleft").innerHTML = "Guesses remaining: " + guessesLeft;
+        document.getElementById("hangword").innerHTML = answerArray.join(" ");
+        document.getElementById("misses").innerHTML = missesArray.join(" ");
+    };
+}
